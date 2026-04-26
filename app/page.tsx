fdf963858f65a731185264883240ea0a5924ebd3
Umut Duz <umutduz5454@gmail.com>
2026-04-26 22:08:08 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -124,7 +124,6 @@ export default function SvetraChatPage() {
 
     const handleResetChat = () => {
         setMessages([]);
-        setSettingsOpen(false);
     };
 
     const handleSubmit = async (e?: React.FormEvent, directText?: string) => {
